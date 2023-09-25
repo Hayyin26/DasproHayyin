463fe5143dd2337be9271684525a21e2c0ba52f7
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-09-25 13:20:19 +0700

    SMS 1 modif

--- Minggu5/PemilihanPercobaan125.java
+++ Minggu5/PemilihanPercobaan125.java
@@ -9,10 +9,13 @@ public class PemilihanPercobaan125 {
         System.out.print("Masukkan angka: ");
         int angka = input25.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka " + angka + " bilangan genap");
-        else
-            System.out.println("Angka " + angka + " bilangan ganjil");
+        // if (angka % 2 == 0)
+        // System.out.println("Angka " + angka + " bilangan genap");
+        // else
+        // System.out.println("Angka " + angka + " bilangan ganjil");
+
+        String output = angka % 2 == 0 ? "Angka " + angka + " bilangan genap" : "Angka " + angka + " bilangan ganjil";
+        System.out.println(output);
 
     }
 }
