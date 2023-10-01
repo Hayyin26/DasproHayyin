ab72ca8a35f51934d5b28d2356aa43553f02ab10
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-02 00:38:45 +0700

    Modif SMS 1

--- Minggu5/PemilihanPercobaan325.java
+++ Minggu5/PemilihanPercobaan325.java
@@ -20,7 +20,7 @@ public class PemilihanPercobaan325 {
             case '+':
                 hasil = angka1 + angka2;
                 System.out.println(angka1 + " + " + angka2 + "=" + hasil);
-                break;
+                //break;
             case '-':
                 hasil = angka1 - angka2;
                 System.out.println(angka1 + " - " + angka2 + "=" + hasil);
