293527f106d92f191c82b8cc8037c8afd46c08a9
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-02 14:38:08 +0700

    Modif SMS 1

--- Minggu6/Pemilihan2Percobaan125.java
+++ Minggu6/Pemilihan2Percobaan125.java
@@ -13,10 +13,15 @@ public class Pemilihan2Percobaan125 {
         tahun = input25.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0) {
                 ;
-            System.out.println("Tahun kabisat");
-        } else
+                System.out.println("Tahun kabisat");
+            } else {
+                System.out.println("Bukan Tahun kabisat");
+            }
+        } else {
             System.out.println("Bukan Tahun kabisat");
+
+        }
     }
 }
