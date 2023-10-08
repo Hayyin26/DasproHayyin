bd52aae77089531cc790324b5eaa99781ba7f453
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-09 00:10:17 +0700

    Modif SMS 1

--- Minggu6/Pemilihan2Percobaan325.java
+++ Minggu6/Pemilihan2Percobaan325.java
@@ -25,7 +25,7 @@ public class Pemilihan2Percobaan325 {
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("penghasilan Bersih : " + gajiBersih);
 
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000) {
                 pajak = 0.15;
             } else if (penghasilan <= 3500000) {
@@ -34,7 +34,8 @@ public class Pemilihan2Percobaan325 {
                 pajak = 0.25;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.println("Penghasil Bersih : " + gajiBersih);
-        } else
-            System.out.println("Masukkan kategori Salah");
+            // } else
+            // System.out.println("Masukkan kategori Salah");
+        }
     }
 }
