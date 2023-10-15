2d18dde74b88041789c47487fa03f62c468a52a5
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-15 22:50:47 +0700

    SMS 1

---
+++ Minggu7/WhileGaji25.java
@@ -0,0 +1,41 @@
+package Minggu7;
+
+import java.util.Scanner;
+
+public class WhileGaji25 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+
+        while (i < jumlahKaryawan) {
+            System.out.println("pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i + 1) + ": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajLembur = jumlahJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajLembur;
+
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+
+        }
+
+    }
+}
