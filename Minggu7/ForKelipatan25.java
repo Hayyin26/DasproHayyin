241310e9ce96947fe5cf8c342241dbd22d3a4838
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-15 21:10:53 +0700

    Modif SMS 1

--- Minggu7/ForKelipatan25.java
+++ Minggu7/ForKelipatan25.java
@@ -6,7 +6,8 @@ public class ForKelipatan25 {
     public static void main(String[] args) {
         Scanner input25 = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0;
+        int kelipatan, rataRata;
+        int jumlah = 0, counter = 0;
 
         System.out.print("Masukkan  bilangan kelipatan kelipatan (1-9): ");
         kelipatan = input25.nextInt();
@@ -16,8 +17,10 @@ public class ForKelipatan25 {
                 counter++;
             }
         }
+        rataRata = jumlah / counter;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rataRata);
 
     }
 }
