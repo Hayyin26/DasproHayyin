c96398c557f750c9d05e5b8d48dc566e1026f27c
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-15 22:35:21 +0700

    SMS 1

---
+++ Minggu7/WhileKelipatan25.java
@@ -0,0 +1,29 @@
+package Minggu7;
+
+import java.util.Scanner;
+
+public class WhileKelipatan25 {
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+
+        int kelipatan, rataRata;
+        int jumlah = 0, counter = 0, i = 1;
+
+        System.out.print("Masukkan  bilangan kelipatan kelipatan (1-9): ");
+        kelipatan = input25.nextInt();
+
+        while (i <= 50) {
+            if (i % kelipatan == 0)
+                ;
+            {
+                jumlah += i;
+                counter++;
+            }
+            i++;
+        }
+        rataRata = (jumlah / counter);
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rataRata);
+    }
+}
