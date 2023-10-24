62532cda512b06c9c840f45153372e34ec884618
Ramadhani Bi Hayyin <143438241+Hayyin26@users.noreply.github.com>
2023-10-24 19:41:15 +0700

    SMS 1

---
+++ Minggu9/ArrayBilangan25.java
@@ -0,0 +1,21 @@
+package Minggu9;
+
+import java.util.Scanner;
+
+public class ArrayBilangan25 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] bil = new int[4];
+
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
